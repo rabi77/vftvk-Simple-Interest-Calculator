005a61e9a7036ae1d451632a37657169c2bfdd6f
rabi <rabisingh.19@nshm.edu.in>
2021-07-08 18:21:03 +0000

    error rectified

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var principal = 0;
-principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var amount = principal * years * rate / 100;
-var year = new Date().getFullYear() + parseInt(years);
+
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval+"%";
 }
 
 
 function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
     if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
