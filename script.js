86482a01c4186661517133cf8b62bb0f9dff97b3
rabi <rabisingh.19@nshm.edu.in>
2021-07-08 17:14:19 +0000

    error rectified

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
-var interest = principal * years * rate / 100;
+var amount = principal * years * rate / 100;
 var year = new Date().getFullYear() + parseInt(years);
 function updateRate() {
     var rateval = document.getElementById("rate").value;
@@ -10,6 +10,11 @@ function updateRate() {
 
 
 function compute() {
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>";
-
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else {
+        document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>";
+    }
 }
